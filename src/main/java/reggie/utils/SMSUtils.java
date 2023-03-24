7d1f4568fc1836e39c5df6951853ee9dba357e5b
lww <2458581048@qq.com>
2023-03-24 14:41:32 +0800

    进行Redis优化

--- src/main/java/reggie/utils/SMSUtils.java
+++ src/main/java/reggie/utils/SMSUtils.java
@@ -24,9 +24,7 @@ public class SMSUtils {
      * @param param 参数
      */
     public static void sendMessage(String signName, String templateCode, String phoneNumbers, String param) {
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou",
-                "LTAI5tQfkvszvtHYBF24uNGS",
-                "BjifxbIqewVPRQSXawSFMDjurybCO0");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         SendSmsRequest request = new SendSmsRequest();
